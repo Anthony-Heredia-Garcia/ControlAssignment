1f88e55153207f0492cff87b99bd6c2335588d1b
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-03-01 15:35:28 -0500

    first commit

---
+++ assignment.js
@@ -0,0 +1,9 @@
+const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+
+if (randomNumber > 0.7){
+    alert ("Big Number!");
+}
+console.log(randomNumber);  
+
+const myArray = [1, 2, 3, 4, 5,];
