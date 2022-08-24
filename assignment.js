69cf9e1325d482decabe09159b1580d1eb764edd
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-08-24 12:41:59 -0400

    review of loops and conditions

--- assignment.js
+++ assignment.js
@@ -1,9 +1,29 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+const myArray = [8, 6, 7, 5, 3, 0, 9];
+const newRandomNumber = Math.random();
 
 
-if (randomNumber > 0.7){
-    alert ("Big Number!");
+// if (randomNumber > 0.7) {
+//     alert("Big Number Generated");
+//     console.log(randomNumber);
+// }
+
+if ((randomNumber > 0.7 && newRandomNumber > 0.7) || (randomNumber < 0.2 || newRandomNumber < 0.2)) {
+    alert("Something Happened");
+    console.log(randomNumber, newRandomNumber);
 }
-console.log(randomNumber);  
 
-const myArray = [1, 2, 3, 4, 5,];
+function regLoop(array) {
+    for (let i = (array.length-1); i >= 0; i--) {
+        console.log(array[i]);
+    }
+}
+
+function notRegLoop() {
+    for (const num of myArray){
+        console.log(num);
+    }
+}
+
+regLoop(myArray);
+notRegLoop();
